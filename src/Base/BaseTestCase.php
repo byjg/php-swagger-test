e1b744f1426c6bb8838b09ac123aabb7eadb2e28
Joao Gilberto Magalhaes <joao@byjg.com.br>
2020-01-21 01:16:23 -0600

    Add ApiTestCaseNew to support PHPUnit 8.5 Issue #35

---
+++ src/Base/BaseTestCase.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace ByJG\ApiTools\Base;
+
+use ByJG\ApiTools\ApiRequester;
+use ByJG\ApiTools\Base\Schema;
+use ByJG\ApiTools\Exception\DefinitionNotFoundException;
+use ByJG\ApiTools\Exception\HttpMethodNotFoundException;
+use ByJG\ApiTools\Exception\InvalidDefinitionException;
+use ByJG\ApiTools\Exception\NotMatchedException;
+use ByJG\ApiTools\Exception\PathNotFoundException;
+use ByJG\ApiTools\Exception\StatusCodeNotMatchedException;
+use ByJG\ApiTools\Exception\GenericSwaggerException;
+use GuzzleHttp\GuzzleException;
+use PHPUnit\Framework\TestCase;
+
+abstract class BaseTestCase extends TestCase
+{
+    /**
+     * @var Schema
+     */
+    protected $swaggerSchema;
+
+    protected $filePath;
+
+    /**
+     * @param string $method The HTTP Method: GET, PUT, DELETE, POST, etc
+     * @param string $path The REST path call
+     * @param int $statusExpected
+     * @param array|null $query
+     * @param array|null $requestBody
+     * @param array $requestHeader
+     * @return mixed
+     * @throws DefinitionNotFoundException
+     * @throws HttpMethodNotFoundException
+     * @throws InvalidDefinitionException
+     * @throws NotMatchedException
+     * @throws PathNotFoundException
+     * @throws StatusCodeNotMatchedException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @deprecated Use assertRequest instead
+     */
+    protected function makeRequest(
+        $method,
+        $path,
+        $statusExpected = 200,
+        $query = null,
+        $requestBody = null,
+        $requestHeader = []
+    ) {
+        $requester = new ApiRequester();
+        $body = $requester
+            ->withSwaggerSchema($this->swaggerSchema)
+            ->withMethod($method)
+            ->withPath($path)
+            ->withQuery($query)
+            ->withRequestBody($requestBody)
+            ->withRequestHeader($requestHeader)
+            ->assertResponseCode($statusExpected)
+            ->send();
+
+        // Note:
+        // This code is only reached if the send is successful and
+        // all matches are satisfied. Otherwise an error is throwed before
+        // reach this
+        $this->assertTrue(true);
+
+        return $body;
+    }
+
+    /**
+     * @param ApiRequester $request
+     * @return mixed
+     * @throws DefinitionNotFoundException
+     * @throws HttpMethodNotFoundException
+     * @throws InvalidDefinitionException
+     * @throws NotMatchedException
+     * @throws PathNotFoundException
+     * @throws StatusCodeNotMatchedException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function assertRequest(ApiRequester $request)
+    {
+        // Add own swagger if nothing is passed.
+        if (!$request->hasSwaggerSchema()) {
+            $request->withSwaggerSchema($this->swaggerSchema);
+        }
+
+        // Request based on the Swagger Request definitios
+        $body = $request->send();
+
+        // Note:
+        // This code is only reached if the send is successful and
+        // all matches are satisfied. Otherwise an error is throwed before
+        // reach this
+        $this->assertTrue(true);
+
+        return $body;
+    }
+}
