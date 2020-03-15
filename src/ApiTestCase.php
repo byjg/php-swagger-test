35110a8b8ecb6982d28fe56a097bddb14e02e422
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2020-03-15 23:02:53 +0100

    ApiTestCase: Make the path to the schema file non-mandatory.
    Two reasons:
    
     - You can configure a schema for a requester, in which case it just isn't needed.
     - You may wish to use a different fileformat or generate the schema in-memory instead.

--- src/ApiTestCase.php
+++ src/ApiTestCase.php
@@ -9,16 +9,19 @@ use GuzzleHttp\GuzzleException;
 
 abstract class ApiTestCase extends BaseTestCase
 {
+    /** @var string|null */
+    protected $filePath;
 
     /**
      * @throws GenericSwaggerException
      */
     protected function setUp()
     {
-        if (empty($this->filePath)) {
-            throw new GenericSwaggerException('You have to define the property $filePath');
+        // load and configure the schema if a path is set
+        if (!empty($this->filePath)) {
+            $data = file_get_contents($this->filePath);
+            $schema = Schema::getInstance($data);
+            $this->setSchema($schema);
         }
-
-        $this->schema = Schema::getInstance(file_get_contents($this->filePath));
     }
 }
