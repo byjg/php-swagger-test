cd5d2c77d860de93d7bc7ffc3536a32e86a93e80
Joao Gilberto Magalhaes <joao@byjg.com.br>
2025-11-15 14:10:33 -0500

    Update XML response validation for improved category and status checks
    Refined test assertions to validate specific category attributes (`id` and `name`) and `status` values in XML responses, ensuring stricter verification of expected response structure.

--- tests/OpenApiTestCaseTest.php
+++ tests/OpenApiTestCaseTest.php
@@ -2,17 +2,7 @@
 
 namespace Tests;
 
-use ByJG\ApiTools\ApiRequester;
 use ByJG\ApiTools\Base\Schema;
-use ByJG\ApiTools\Exception\DefinitionNotFoundException;
-use ByJG\ApiTools\Exception\GenericApiException;
-use ByJG\ApiTools\Exception\HttpMethodNotFoundException;
-use ByJG\ApiTools\Exception\InvalidDefinitionException;
-use ByJG\ApiTools\Exception\InvalidRequestException;
-use ByJG\ApiTools\Exception\NotMatchedException;
-use ByJG\ApiTools\Exception\PathNotFoundException;
-use ByJG\ApiTools\Exception\RequiredArgumentNotFound;
-use ByJG\ApiTools\Exception\StatusCodeNotMatchedException;
 
 class OpenApiTestCaseTest extends TestingTestCase
 {
@@ -58,11 +48,10 @@ class OpenApiTestCaseTest extends TestingTestCase
         $this->assertNotFalse($xml, 'Response should be valid XML');
 
         // Verify expected Pet structure in XML
-        $this->assertNotEmpty((string)$xml->id, 'Pet should have an id');
         $this->assertEquals('1', (string)$xml->id, 'Pet id should be 1');
-        $this->assertNotEmpty((string)$xml->name, 'Pet should have a name');
         $this->assertEquals('Doris', (string)$xml->name, 'Pet name should be Doris');
-        $this->assertTrue(isset($xml->category), 'Pet should have a category');
-        $this->assertTrue(isset($xml->status), 'Pet should have a status');
+        $this->assertEquals("101", (string)$xml->category->id, 'Pet should have a category');
+        $this->assertEquals("cat", (string)$xml->category->name, 'Pet should have a category');
+        $this->assertEquals("sold", (string)$xml->status, 'Pet should have a status');
     }
 }
